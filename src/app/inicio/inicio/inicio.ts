11df33fec61aaf47d32ce3d9702d458c26357ecf
sebatevez91 <Sebastevez91@gmail.com>
2026-06-13 12:15:57 -0300

    fix: modifico estilos

--- src/app/inicio/inicio/inicio.ts
+++ src/app/inicio/inicio/inicio.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
